d8b6185eabf86f83e74963d983c5d0ab7ff8c4f1
Alexander Kuziv <makklays@gmail.com>
2024-02-17 23:32:28 +0100

    18-02-2024 Add new functions for 'UserController'.

--- src/main/java/com/techmatrix18/web/api/UserController.java
+++ src/main/java/com/techmatrix18/web/api/UserController.java
@@ -2,33 +2,55 @@ package com.techmatrix18.web.api;
 
 import com.techmatrix18.model.User;
 import com.techmatrix18.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import com.techmatrix18.service.implementation.UserImpl;
 import org.springframework.web.bind.annotation.*;
 
 import javax.xml.bind.ValidationException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
 
     private UserRepository userRepository;
+    private UserImpl userService;
 
     @GetMapping(path = "/")
-    public Iterable<User> getUsers() throws ValidationException {
-        return userRepository.findAll();
+    public List<User> getUsers() throws ValidationException {
+        return userService.getAllUsers();
     }
 
     @PostMapping(path = "/add")
     public @ResponseBody String addUser (@RequestParam String firstname, @RequestParam String lastname, @RequestParam String email, @RequestParam Long positionId) {
-        User n = new User();
-        n.setFirstname(firstname);
-        n.setLastname(lastname);
-        n.setEmail(email);
-        n.setPositionId(positionId);
-        userRepository.save(n);
+        User u = new User();
+        u.setFirstname(firstname);
+        u.setLastname(lastname);
+        u.setEmail(email);
+        u.setPositionId(positionId);
+        userService.addUser(u);
         return "Saved";
+    }
+
+    @PatchMapping(path = "/update")
+    public @ResponseBody String updateUser (@RequestParam Long userId, @RequestParam String firstname, @RequestParam String lastname, @RequestParam String email, @RequestParam Long positionId) {
+        User u = userService.getUserById(userId);
+        if (!u.getEmail().isEmpty()) {
+            u.setFirstname(firstname);
+            u.setLastname(lastname);
+            u.setEmail(email);
+            u.setPositionId(positionId);
+            userService.updateUser(u);
+        }
+        return "Updated";
+    }
+
+    @DeleteMapping(path = "/delete/{userId:\\\\d+}")
+    public @ResponseBody String deleteUser (@PathVariable Long userId) {
+        User u = userService.getUserById(userId);
+        if (!u.getEmail().isEmpty()) {
+            userService.deleteUser(userId);
+        }
+        return "Deleted";
     }
 }
 
