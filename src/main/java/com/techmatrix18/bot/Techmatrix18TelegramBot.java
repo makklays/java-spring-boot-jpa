9cbfbbf70d77ac43e914ece6720a6a06c49774fe
Alexander Kuziv <makklays@gmail.com>
2024-02-21 22:29:48 +0100

    21-02-2024 Add 'Telegram Bot' (dependency, application.properties and class).

---
+++ src/main/java/com/techmatrix18/bot/Techmatrix18TelegramBot.java
@@ -0,0 +1,57 @@
+package com.techmatrix18.bot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+/**
+ * Telegrambot for Javarush Community from Javarush community.
+ */
+@Component
+public class Techmatrix18TelegramBot extends TelegramLongPollingBot {
+
+    @Value("${bot.username}")
+    private String username;
+
+    @Value("${bot.token}")
+    private String token;
+
+    @Override
+    public void onUpdateReceived(Update update) { // here point for all messages
+        if(update.hasMessage() && update.getMessage().hasText()) {
+            String message = update.getMessage().getText().trim();
+            String chatId = update.getMessage().getChatId().toString();
+
+            SendMessage sm = new SendMessage();
+
+            if (message == "¿Como me llama?") {
+                sm.setChatId(chatId);
+                sm.setText("Tu nombre saben todos usuarios, Neo! Ve por conejo blanco!");
+            } else {
+                sm.setChatId(chatId);
+                sm.setText(message);
+            }
+
+            try {
+                execute(sm);
+            } catch (TelegramApiException e) {
+                //todo add logging to the project.
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return username;
+    }
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+}
+
