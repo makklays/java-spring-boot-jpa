264aa97486218ec5f71c48784f4094e14895aa18
Alexander Kuziv <makklays@gmail.com>
2024-06-26 22:55:56 +0200

    26-06-2024 Add new pole 'roles' for table 'users'.

--- src/main/resources/db/migration/V2_1__Update_users_table.sql
+++ src/main/resources/db/migration/V2_1__Update_users_table.sql
@@ -1,5 +1,5 @@
 
-INSERT INTO `users` (`firstname`, `lastname`, `email`, `password`, `bio`, `position_id`, `created_at`, `updated_at`) VALUE ('Alexander', 'Kuziv', 'a.kuziv@techmatrix18.com', 'password11', 'My bio.. ', 1, '2024-02-21 18:19:03', '2024-02-21 18:19:03');
-INSERT INTO `users` (`firstname`, `lastname`, `email`, `password`, `bio`, `position_id`, `created_at`, `updated_at`) VALUE ('Alexander', 'Kuziv', 'a.kuziv2@techmatrix18.com', 'password22', 'My bio 222.. ', 2, '2024-02-21 18:19:03', '2024-02-21 18:19:03');
-INSERT INTO `users` (`firstname`, `lastname`, `email`, `password`, `bio`, `position_id`, `created_at`, `updated_at`) VALUE ('Alexander', 'Kuziv', 'a.kuziv3@techmatrix18.com', 'password33', 'My bio 33.. ', 3, '2024-02-21 18:19:03', '2024-02-21 18:19:03');
+INSERT INTO `users` (`firstname`, `lastname`, `email`, `password`, `bio`, `roles`, `position_id`, `created_at`, `updated_at`) VALUE ('Alexander', 'Kuziv', 'a.kuziv@techmatrix18.com', 'password11', 'My bio.. ', 'USER', 1, '2024-02-21 18:19:03', '2024-02-21 18:19:03');
+INSERT INTO `users` (`firstname`, `lastname`, `email`, `password`, `bio`, `roles`, `position_id`, `created_at`, `updated_at`) VALUE ('Alexander', 'Kuziv', 'a.kuziv2@techmatrix18.com', 'password22', 'My bio 222.. ', 'USER', 2, '2024-02-21 18:19:03', '2024-02-21 18:19:03');
+INSERT INTO `users` (`firstname`, `lastname`, `email`, `password`, `bio`, `roles`, `position_id`, `created_at`, `updated_at`) VALUE ('Alexander', 'Kuziv', 'a.kuziv3@techmatrix18.com', 'password33', 'My bio 33.. ', 'ADMIN', 3, '2024-02-21 18:19:03', '2024-02-21 18:19:03');
 
