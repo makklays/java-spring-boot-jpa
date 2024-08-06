0943e9d5e3853c76600fa6a5d5289495356e678a
Alexander Kuziv <makklays@gmail.com>
2024-08-06 20:21:41 +0200

    06-08-2024 Add new views for 'Registration'.

--- src/main/java/com/techmatrix18/web/views/AuthViewsController.java
+++ src/main/java/com/techmatrix18/web/views/AuthViewsController.java
@@ -1,13 +1,16 @@
 package com.techmatrix18.web.views;
 
+import com.techmatrix18.model.Role;
 import com.techmatrix18.model.User;
 import com.techmatrix18.service.PositionService;
+import com.techmatrix18.service.RoleService;
 import com.techmatrix18.service.UserService;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -21,10 +24,12 @@ public class AuthViewsController {
 
     private final UserService userService;
     private final PositionService positionService;
+    private final RoleService roleService;
 
-    public AuthViewsController(UserService userService, PositionService positionService) {
+    public AuthViewsController(UserService userService, PositionService positionService, RoleService roleService) {
         this.userService = userService;
         this.positionService = positionService;
+        this.roleService = roleService;
     }
 
     @GetMapping("/")
@@ -72,11 +77,21 @@ public class AuthViewsController {
     }
 
     @PostMapping("/my-registr-post")
-    public String registrPost(Model model, @Valid User user, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return "auth/registr";
-        }
+    public String registrPost(Model model, User user, BindingResult bindingResult) {
+        //if (bindingResult.hasErrors()) {
+        //    return "auth/registr";
+        //}
 
+        Role role = new Role();
+        role.setName("ROLE_USER");
+        roleService.createRole(role);
+
+        String password = user.getPassword();
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String result = encoder.encode(password);
+
+        user.setPassword(result);
         userService.addUser(user);
 
         return "redirect:/my-login";
