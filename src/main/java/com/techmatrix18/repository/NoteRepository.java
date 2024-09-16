ee4ef254959eb1233c4064ee6c3ae5c7a652c1c7
Alexander Kuziv <makklays@gmail.com>
2024-09-16 23:00:27 +0200

    16-09-2024 Add new entity 'Note'.

--- src/main/java/com/techmatrix18/repository/NoteRepository.java
+++ src/main/java/com/techmatrix18/repository/NoteRepository.java
@@ -1,4 +1,11 @@
 package com.techmatrix18.repository;
 
-public class NoteRepository {
+import com.techmatrix18.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NoteRepository extends JpaRepository<Note, Long> {
+    List<Note> findByUserId(Long userId);
 }
+
