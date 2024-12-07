5f30f0ea1eba5c9426b5450c21816108a8cbc656
Alexander Kuziv <makklays@gmail.com>
2024-12-07 23:19:40 +0100

    07-12-2024 Add new entity 'Card' for REST API NBU - National Bank Ukraine.

---
+++ src/main/java/com/techmatrix18/service/impl/CardServiceImpl.java
@@ -0,0 +1,81 @@
+package com.techmatrix18.service.impl;
+
+import com.techmatrix18.model.Card;
+import com.techmatrix18.repository.CardRepository;
+import com.techmatrix18.service.CardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Implementation of {@link CardService} interface.
+ * for REST API NBU - National Bank Ukraine
+ *
+ * @author Alexander Kuziv
+ * @version 1.0
+ * @since 07-12-2024
+ */
+
+@Service
+public class CardServiceImpl implements CardService {
+    @Lazy
+    @Autowired
+    private CardRepository cardRepository;
+
+    @Override
+    public List<Card> getAllCards() {
+        List<Card> list = new ArrayList<>();
+        cardRepository.findAll().forEach(list::add);
+
+        return list;
+    }
+
+    @Override
+    public Optional<Card> getCardById(Long id) {
+        return cardRepository.findById(id);
+    }
+
+    @Override
+    public boolean addCard(Card card) {
+        Card c = cardRepository.save(card);
+        if (!c.getTitle().isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean updateCard(Card card) {
+        Card c = cardRepository.save(card);
+        if (!c.getTitle().isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean deleteCard(Long id) {
+        Card card = cardRepository.getById(id);
+        if (!card.getTitle().isEmpty()) {
+            cardRepository.delete(card);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public Page<Card> findPaginated(int pageNo, int pageSize) {
+        Pageable pageable = PageRequest.of(pageNo - 1, pageSize);
+        return cardRepository.findAll(pageable);
+    }
+}
+
---
+++ src/main/java/com/techmatrix18/web/api/CardController.java
@@ -0,0 +1,111 @@
+package com.techmatrix18.web.api;
+
+import com.techmatrix18.model.Card;
+import com.techmatrix18.model.User;
+import com.techmatrix18.service.CardService;
+import com.techmatrix18.service.UserService;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+import javax.xml.bind.ValidationException;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Simple rest-controller for Card
+ * for REST API NBU - National Bank Ukraine
+ *
+ * @author Alexander Kuziv
+ * @version 1.0
+ * @since 07-12-2024
+ */
+
+@RestController
+@RequestMapping("/api/v1/card")
+public class CardController {
+
+    private final CardService cardService;
+    private final UserService userService;
+
+    public CardController(CardService cardService, UserService userService) {
+        this.cardService = cardService;
+        this.userService = userService;
+    }
+
+    @GetMapping(path = "/all", produces = "application/json;charset=UTF-8")
+    public List<Card> getCards() throws ValidationException {
+        return cardService.getAllCards();
+    }
+
+    @GetMapping(path = "/{id}", produces = "application/json;charset=UTF-8")
+    public Object getOne(@PathVariable String id) throws ValidationException {
+        Long cardId = Long.parseLong(id);
+        Optional<Card> card = cardService.getCardById(cardId);
+        if (card.isPresent()) {
+            return card.get();
+        } else {
+            return "{\"status\": \"error\", \"message\": \"Didn't find card with ID=" + id + "\"}";
+        }
+    }
+
+    @GetMapping(path = "/page", produces = "application/json;charset=UTF-8")
+    public Object getPage(@RequestParam String pageNo, @RequestParam String pageSize) {
+        int pNo = Integer.parseInt(pageNo);
+        int pSize = Integer.parseInt(pageSize);
+
+        Page<Card> cards = cardService.findPaginated(pNo, pSize);
+        if (cards != null) {
+            return cards;
+        } else {
+            return "{\"status\": \"error\", \"message\": \"Didn't find cards with pageNo=" + pageNo + " and pageSize=" + pageSize + " \"}";
+        }
+    }
+
+    @PostMapping(path = "/add", produces = "application/json;charset=UTF-8")
+    public @ResponseBody String addCard (@RequestParam String title, @RequestParam int number, @RequestParam String cc, @RequestParam String month, @RequestParam String year, @RequestParam String cvv, @RequestParam String type, @RequestParam String amount, @RequestParam String userId) {
+        // user
+        User user = userService.getUserById(Long.parseLong(userId));
+
+        // card
+        Card c = new Card();
+        c.setTitle(title);
+        c.setNumber(number);
+        c.setMonth(Integer.parseInt(month));
+        c.setYear(Integer.parseInt(year));
+        c.setCvv(Integer.parseInt(cvv));
+        c.setType(type);
+        c.setAmount(new BigDecimal(amount));
+        c.setUser(user);
+        c.setCc(cc);
+        cardService.addCard(c);
+
+        return "{\"status\": \"success\", \"message\": \"Card saved successfully!\"}";
+    }
+
+    @PatchMapping(path = "/update", produces = "application/json;charset=UTF-8")
+    public @ResponseBody String updateCard (@RequestParam String cardId, @RequestParam String title, @RequestParam int number, @RequestParam String cc, @RequestParam String month, @RequestParam String year, @RequestParam String cvv, @RequestParam String type, @RequestParam String amount, @RequestParam String userId) {
+        Optional<Card> c = cardService.getCardById(Long.parseLong(cardId));
+        if (c.isPresent()) {
+
+            // user
+            User user = userService.getUserById(Long.parseLong(userId));
+
+            // card
+            c.get().setTitle(title);
+            c.get().setNumber(number);
+            c.get().setMonth(Integer.parseInt(month));
+            c.get().setYear(Integer.parseInt(year));
+            c.get().setCvv(Integer.parseInt(cvv));
+            c.get().setType(type);
+            c.get().setAmount(new BigDecimal(amount));
+            c.get().setUser(user);
+            c.get().setCc(cc);
+            cardService.updateCard(c.get());
+
+            return "{\"status\": \"success\", \"message\": \"Card updated successfully!\"}";
+        } else {
+            return "{\"status\": \"error\", \"message\": \"Didn't find card with ID=" + cardId + "\"}";
+        }
+    }
+}
+
