a2574abd594f9f6ac6aea7e658eb1eb95fc014a7
Alexander Kuziv <makklays@gmail.com>
2024-12-09 23:09:25 +0100

    10-12-2024 Add new entity 'CardTransaction' and pages for REST API NBU - National Bank Ukraine.

---
+++ src/main/java/com/techmatrix18/repository/CardTransactionRepository.java
@@ -0,0 +1,19 @@
+package com.techmatrix18.repository;
+
+import com.techmatrix18.model.CardTransaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface CardTransactionRepository extends JpaRepository<CardTransaction, Long> {
+
+    CardTransaction getById(Long id);
+
+    List<CardTransaction> findByTitle(String title);
+
+    Page<CardTransaction> findAll(Pageable pageable);
+}
+
